9921ca04289a6129003f18aa45274c9f107effe6
Heba Derawi <heba.derawi95@hotmail.com>
2023-11-13 15:04:01 +0100

    changes

--- Program.cs
+++ Program.cs
@@ -1,26 +1,41 @@
-﻿namespace Grupparbete
+﻿using System.Reflection.Metadata;
+
+namespace Grupparbete
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            User user = new User();
+            Login user = new Login();
             user.Input();
             
         }
 
-        public class User
+        public class Login
         {
             public string Username { get; set; }
             public string Password { get; set; }
 
-            public void Input () 
+            public string Input () 
             {
-                Console.WriteLine("Username: ");
-                Username = Console.ReadLine();
-                Console.WriteLine("Password: ");
-                Password = Console.ReadLine();
+                    Console.WriteLine("Username: ");
+                    Username = Console.ReadLine();
+                    Console.WriteLine("Password: ");
+                    Password = Console.ReadLine();
+                
+                return Username;
+                return Password;
             }    
+
+        }
+
+        public class User
+        {
+
+        }
+        public class Admin
+        {
+
         }
     }
 }
